4d29e18dbf95185160f7aec5e262787622fbe33c
Sree Lakshmi Dileep <sreelakshmidileep2004@gmail.com>
2025-10-04 12:35:30 +0530

    feat:solar system added in home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import DeepZoomViewer from './DeepZoomViewer';
 import DragDeep from './DragDeep';
+import SolarSystem from './components/SolarSystem'; // import your 3D solar system
+
 function App() {
   return (
     <div className="App">
-      {/* <DeepZoomViewer/> */}
-      <DragDeep/>
+      {/* Solar System as landing page */}
+      <SolarSystem />
+
+      {/* Keep your existing components */}
+      {/* <DeepZoomViewer /> */}
+      <DragDeep />
     </div>
   );
 }
