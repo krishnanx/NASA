100eea99aac84c2fed010e3f713072d1610759df
krishnanx <abhishekkrishnan2006@gmail.com>
2025-10-05 07:57:19 +0530

    Planets are not unmounted when went to a diff page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import DeepZoomViewer from "./DeepZoomViewer";
 import DragDeep from "./DragDeep";
@@ -6,21 +6,29 @@ import SolarSystem from "./components/SolarSystem";
 import Loader from "./components/Loader"; // renamed for consistency
 import MarsExplore from './pages/MarsExplore'
 import PlanetModel from "./components/PlanetsModel"
-import { Route,Routes } from "react-router-dom";
+import { Route,Routes,useLocation } from "react-router-dom";
 function App() {
-  
+  const location = useLocation();
+
   return (
-    <div className="App">
-     
-        <>
-          {/* Solar System as landing page */}
-        <Routes>
-          <Route path="/" element={<SolarSystem />} />
-          <Route path="/planet" element={<PlanetModel />} />
-          <Route path="/planet/explore" element={<MarsExplore />} />
-          <Route path="/planets/deepzoom" element={<DragDeep />} />
-        </Routes>
-        </>
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
+      {/* SolarSystem is always mounted */}
+      <div
+        style={{
+          display: location.pathname === "/" ? "block" : "none",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <SolarSystem />
+      </div>
+
+      {/* Other routes */}
+      <Routes>
+        <Route path="/planet" element={<PlanetModel />} />
+        <Route path="/planet/explore" element={<MarsExplore />} />
+        <Route path="/planets/deepzoom" element={<DragDeep />} />
+      </Routes>
     </div>
   );
 }
